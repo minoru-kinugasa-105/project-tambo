492ec90e5c312975c5a36714edce2bfeeb99df7e
衣笠みのる <168079463+minoru-kinugasa-105@users.noreply.github.com>
2024-12-17 10:27:42 +0900

    レスポンシブ対応

---
+++ asetts/javascript/index.js
@@ -0,0 +1,34 @@
+// mobile-header movement
+const mobileBodyElement = document.getElementById("mobile-body");
+const mobileBodyBtn = document.getElementById("mobile-body-btn");
+const [line1, line2, line3] = document.querySelectorAll(".line");
+
+mobileBodyBtn.addEventListener("click", () => {
+    if (mobileBodyElement.style.display === "flex") {
+        mobileBodyElement.style.display = "none";
+        line1.style.transform = "rotate(0deg)";
+        line2.style.transform = "rotate(0deg)";
+        line2.style.marginTop = "0";
+        line3.style.display = "block";
+    } else {
+        mobileBodyElement.style.display = "flex";
+        line1.style.transform = "rotate(45deg)";
+        line2.style.transform = "rotate(-45deg)";
+        line2.style.marginTop = "-14px";
+        line3.style.display = "none";
+    }
+});
+
+// view-more movement
+const voiceMoreElement = document.getElementById("voice-more");
+const voiceMoreBtn = document.getElementById("voice-more-btn");
+
+voiceMoreBtn.addEventListener("click", () => {
+    if (voiceMoreElement.style.display === "flex") {
+        voiceMoreElement.style.display = "none";
+        voiceMoreBtn.textContent = "もっとみる";
+    } else {
+        voiceMoreElement.style.display = "flex";
+        voiceMoreBtn.textContent = "閉じる";
+    }
+});
